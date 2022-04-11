eedd537b591837eb92ec9ef971d13d8669931e78
Gunnar Behrens <gunnar.behrens@dlr.de>
2022-04-11 10:44:13 +0200

    evaluate performance of VAE with BN8 for train, val, test data set with Rasp_18 output scaling

---
+++ VAE_clim_clim_conv_BN8_per_3months_PNAS_scaling.py
@@ -0,0 +1,375 @@
+"""
+test, validation, training MSEs and biases of VAE with 8 latent nodes in Rasp scaling
+"""
+from tensorflow.keras.layers import Lambda, Input, Dense
+from cbrain.layers import *
+from tensorflow.keras.models import Model
+from tensorflow.keras.losses import mse, binary_crossentropy
+
+from tensorflow.keras import backend as K
+from tensorflow.keras.callbacks import LearningRateScheduler, Callback
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import os
+
+
+import tensorflow as tf
+from cbrain.imports import *
+
+from cbrain.utils import *
+import pandas as ps
+from cbrain.data_generator import DataGenerator
+
+
+### VAE_clim_clim_conv with eight latent nodes 
+
+# reparameterization trick of VAE 
+# instead of sampling from Q(z|X), sample epsilon = N(0,I)
+# z = z_mean + sqrt(var) * epsilon
+def sampling(args):
+    """Reparameterization trick by sampling from an isotropic unit Gaussian.
+
+    # Arguments
+        args (tensor): mean and log of variance of Q(z|X)
+
+    # Returns
+        z (tensor): sampled latent vector
+    based on VAE presented on keras webpage for keras version 1 /
+    recent keras VAE version can be seen on
+    https://keras.io/examples/generative/vae/
+    """
+
+    z_mean, z_log_var = args
+    batch= K.shape(z_mean)[0]
+    dim=K.int_shape(z_mean)[1]
+    # by default, random_normal has mean = 0 and std = 1.0
+    epsilon=K.random_normal(shape=(batch,dim)) # epsilion= random_normal distributed tensor
+    sample_prob=z_mean+K.exp(0.5*z_log_var)*epsilon #exp= elementwise exponential
+    return sample_prob
+
+# kl annealing to improve reproduction skills of VAE 
+klstart = 2
+# number of epochs over which KL scaling is increased from 0 to 1
+kl_annealtime = 5
+
+class AnnealingCallback(Callback):
+    def __init__(self, weight):
+        self.weight = weight
+    def on_epoch_end (self, epoch, logs={}):
+        if epoch > klstart :
+            new_weight = min(K.get_value(self.weight) + (1./kl_annealtime), 1.)
+            K.set_value(self.weight, new_weight)
+        print ("Current KL Weight is " + str(K.get_value(self.weight)))
+
+
+# the starting value of weight is 0
+# define it as a keras backend variable
+weight = K.variable(0.)
+
+    
+original_dim_input=64  # input node size (CAM variables)
+
+original_dim_output=int(65+64) # output node size (SP + CAM variables)
+
+
+# network parameters
+input_shape = (original_dim_input,)
+out_shape=(original_dim_output,)
+intermediate_dim = 463 # nodes in first hidden layers of encoder and last hidden layers of decoder 
+batch_size = 714
+latent_dim = 8 # latent space dimensions
+epochs = 40    
+    
+## Encoder 
+inputs =Input(shape=input_shape, name='encoder_input')
+x_0 =Dense(intermediate_dim, activation='relu')(inputs)
+x_1 =Dense(intermediate_dim, activation='relu')(x_0)
+x_2 =Dense(int(np.round(intermediate_dim/2)), activation='relu')(x_1)
+x_3 =Dense(int(np.round(intermediate_dim/4)), activation='relu')(x_2)
+x_4 =Dense(int(np.round(intermediate_dim/8)), activation='relu')(x_3)
+x_5 =Dense(int(np.round(intermediate_dim/16)), activation='relu')(x_4)
+
+
+
+z_mean = Dense(latent_dim, name='z_mean')(x_5)
+z_log_var = Dense(latent_dim, name='z_log_var')(x_5)
+
+
+
+# reparametrization trick
+z = Lambda(sampling, output_shape=(latent_dim), name='z')([z_mean, z_log_var])
+
+# instantiate encoder model
+encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+encoder.summary()
+
+
+## Decoder
+decoder_inputs =Input(shape=(latent_dim,), name='decoder_input')
+x_1 =Dense(int(np.round(intermediate_dim/16)), activation='relu')(decoder_inputs)
+x_2 =Dense(int(np.round(intermediate_dim/8)), activation='relu')(x_1)
+x_3 =Dense(int(np.round(intermediate_dim/4)), activation='relu')(x_2)
+x_4 =Dense(int(np.round(intermediate_dim/2)), activation='relu')(x_3)
+x_5 =Dense(intermediate_dim, activation='relu')(x_4)
+x_6 =Dense(intermediate_dim, activation='relu')(x_5)
+
+outputs = Dense(original_dim_output, activation='elu')(x_6)
+
+decoder = Model(decoder_inputs, outputs, name='decoder')
+decoder.summary()
+
+emul_outputs=decoder(encoder(inputs)[2])
+
+kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+kl_loss = K.sum(kl_loss, axis=-1)
+kl_loss *= -0.5
+VAE_loss = K.mean(kl_loss*weight)
+
+
+VAE_clim_clim_conv=Model(inputs,emul_outputs)
+VAE_clim_clim_conv.add_loss(VAE_loss)
+VAE_clim_clim_conv.add_metric(kl_loss, name='kl_loss', aggregation='mean')
+
+
+#loading the output normalization scalars for SP variables ( stds over 3 months of SP simulation)
+
+scale_array=ps.read_csv('nn_config/scale_dicts/Scaling_cond_VAE.csv')
+
+
+PHQ_std_surf=scale_array.PHQ_std.values[-1]
+
+TPHYSTND_std_23=scale_array.TPHYSTND_std.values[-1]# for dT/dt we are using the std on level 23 ~ 845 hPa
+
+PRECT_std=scale_array.PRECT_std.values
+FSNS_std=scale_array.FSNS_std.values
+FSNT_std=scale_array.FSNT_std.values
+FLNS_std=scale_array.FLNS_std.values
+FLNT_std=scale_array.FLNT_std.values
+
+# and the CAM variables 
+scale_array_2D=ps.read_csv('nn_config/scale_dicts/Scaling_enc_II_range_profiles.csv')
+scale_array_1D=ps.read_csv('nn_config/scale_dicts/Scaling_enc_II_range.csv')
+
+TBP_std_surf=scale_array_2D.TBP_std.values[-1]
+
+QBP_std_surf=scale_array_2D.QBP_std.values[-1]
+
+Q_lat_std_surf=scale_array_1D.Q_lat_std.values
+
+Q_sens_std_surf=scale_array_1D.Q_sens_std.values
+
+
+Q_solar_std_surf=scale_array_1D.Q_sol_std.values
+
+PS_std_surf=scale_array_1D.PS_std.values
+
+
+# defining the scaling dict for the VAE training 
+
+scale_dict_II = {
+    'PHQ': 1/PHQ_std_surf, 
+    'QBP':1/QBP_std_surf,
+    'TPHYSTND': 1/TPHYSTND_std_23, 
+    'TBP':1/TBP_std_surf,
+    'FSNT': 1/FSNT_std, 
+    'FSNS': 1/FSNS_std, 
+    'FLNT': 1/FLNT_std, 
+    'FLNS': 1/FLNS_std, 
+    'PRECT': 1/PRECT_std, 
+    'LHFLX': 1/Q_lat_std_surf, 
+    'SHFLX': 1/Q_sens_std_surf, 
+    'SOLIN': 1/Q_solar_std_surf,
+    'PS':1/PS_std_surf
+}
+
+in_vars = ['QBP', 'TBP','PS', 'SOLIN', 'SHFLX', 'LHFLX']
+out_vars = ['PHQ','TPHYSTND','FSNT', 'FSNS', 'FLNT', 'FLNS', 'PRECT','QBP', 'TBP','PS', 'SOLIN', 'SHFLX', 'LHFLX']
+out_vars_PNAS = ['PHQ','TPHYSTND','FSNT', 'FSNS', 'FLNT', 'FLNS', 'PRECT']
+
+# Takes representative value for PS since purpose is normalization
+PS = 1e5; P0 = 1e5;
+P = P0*hyai+PS*hybi; # Total pressure [Pa]
+dP = P[1:]-P[:-1];
+
+## test data: October, November, December of first year of SPCAM simulations
+
+val_gen_II = DataGenerator(
+    data_fn = '../preprocessed_data/1918_train_3_month_OND.nc',
+    input_vars = in_vars,
+    output_vars = out_vars,
+    norm_fn = '../preprocessed_data/000_norm_1_month.nc',
+    input_transform = ('mean', 'maxrs'),
+    output_transform = scale_dict_II,
+    batch_size=8192,
+    shuffle=True
+)
+
+scale_dict_pnas= pickle.load(open('nn_config/scale_dicts/002_pnas_scaling.pkl','rb'))
+
+
+## test data with Rasp normalization 
+val_gen_I = DataGenerator(
+    data_fn = '../preprocessed_data/1918_train_3_month_OND.nc',
+    input_vars = in_vars,
+    output_vars = out_vars_PNAS,
+    norm_fn = '../preprocessed_data/001_norm.nc',
+    input_transform = ('mean', 'maxrs'),
+    output_transform = scale_dict_pnas,
+    batch_size=8192,
+    shuffle=True
+)
+
+
+VAE_clim_clim_conv.compile(tf.keras.optimizers.Adam(lr=1E-4), loss=mse, metrics=['mse'])
+
+VAE_clim_clim_conv.load_weights('./saved_models/VAE_clim_clim_conv/VAE_clim_clim_conv_BN8_40_opt_anneal.h5')
+
+
+## test data set 
+
+
+time_max=4415 # last time step 
+#was previously calculated based on length of array in *train*.nc divided by lat.size*lon.size
+#(8192 samples per time step)
+
+X=np.nan*np.zeros((time_max,8192,64))
+Y=np.nan*np.zeros((time_max,8192,129))
+Y_emul=np.nan*np.zeros((time_max,8192,129))
+print('start test pred')
+
+for i in tqdm(np.arange(X[:,1,1].size)):
+    X[i],Y[i]=val_gen_II[i]
+    Y_emul[i]=VAE_clim_clim_conv.predict(X[i])[:,0:129] 
+    #only predictions of VAE for SP variables [0:65] are later evaluated to compare against reference ANN 
+
+
+    
+    
+X_=np.reshape(X,(time_max*8192,64))
+Y_=val_gen_II.output_transform.inverse_transform(np.reshape(Y,(time_max*8192,129)))
+Y_emul_=val_gen_II.output_transform.inverse_transform(np.reshape(Y_emul,(time_max*8192,129)))
+
+print('End test pred')
+
+mse=np.mean((val_gen_I.output_transform.transform(Y_emul_[:,0:65])-val_gen_I.output_transform.transform(Y_[:,0:65]))**2)
+
+print(mse)
+
+mean_bias=np.mean(val_gen_I.output_transform.transform(Y_emul_[:,0:65])-val_gen_I.output_transform.transform(Y_[:,0:65]))
+print(mean_bias)
+del X, Y, Y_emul
+del X_, Y_, Y_emul_     
+
+## VAE_clim_clim_conv on validation data 
+print('start Validation evaluation')
+
+val_gen_III = DataGenerator(
+    data_fn = '../preprocessed_data/1918_valid_3_month_AMJ.nc',
+    input_vars = in_vars,
+    output_vars = out_vars,
+    norm_fn = '../preprocessed_data/000_norm_1_month.nc',
+    input_transform = ('mean', 'maxrs'),
+    output_transform = scale_dict_II,
+    batch_size=8192,
+    shuffle=True
+)
+
+## in Rasp scaling
+val_gen_IV = DataGenerator(
+    data_fn = '../preprocessed_data/1918_valid_3_month_AMJ.nc',
+    input_vars = in_vars,
+    output_vars = out_vars_PNAS,
+    norm_fn = '../preprocessed_data/000_norm_1_month.nc',
+    input_transform = ('mean', 'maxrs'),
+    output_transform = scale_dict_pnas,
+    batch_size=8192,
+    shuffle=True
+)
+
+time_max=4367 # last time step 
+#was previously calculated based on length of array in *valid*.nc divided by lat.size*lon.size
+#(8192 samples per time step) 
+
+
+val_X=np.nan*np.zeros((time_max,8192,64))
+val_Y=np.nan*np.zeros((time_max,8192,129))
+val_Y_emul=np.nan*np.zeros((time_max,8192,129))
+
+
+for i in tqdm(np.arange(val_X[:,1,1].size)):
+    val_X[i],val_Y[i]=val_gen_III[i]
+    val_Y_emul[i]=VAE_clim_clim_conv.predict(val_X[i])[:,0:129]
+    
+    
+val_X_=np.reshape(val_X,(time_max*8192,64))
+val_Y_=val_gen_III.output_transform.inverse_transform(np.reshape(val_Y,(time_max*8192,129)))
+val_Y_emul_=val_gen_III.output_transform.inverse_transform(np.reshape(val_Y_emul,(time_max*8192,129)))
+
+val_mse=np.mean((val_gen_IV.output_transform.transform(val_Y_emul_[:,0:65])-val_gen_IV.output_transform.transform(val_Y_[:,0:65]))**2)
+print(val_mse)
+mean_bias_val=np.mean((val_gen_IV.output_transform.transform(val_Y_emul_[:,0:65])-val_gen_IV.output_transform.transform(val_Y_[:,0:65])))
+print(mean_bias_val)
+del val_X, val_Y, val_Y_emul
+del val_X_, val_Y_, val_Y_emul_
+
+## VAE_clim_clim_conv on trainin data 
+print('start Training evaluation')
+
+train_gen_IV = DataGenerator(
+    data_fn = '../preprocessed_data/1918_train_3_month_JAS_shuffle.nc',
+    input_vars = in_vars,
+    output_vars = out_vars,
+    norm_fn = '../preprocessed_data/000_norm_1_month.nc',
+    input_transform = ('mean', 'maxrs'),
+    output_transform = scale_dict_II,
+    batch_size=8192,
+    shuffle=True
+)
+## in Rasp scaling
+train_gen_III = DataGenerator(
+    data_fn = '../preprocessed_data/1918_train_3_month_JAS_shuffle.nc',
+    input_vars = in_vars,
+    output_vars = out_vars_PNAS,
+    norm_fn = '../preprocessed_data/001_norm.nc',
+    input_transform = ('mean', 'maxrs'),
+    output_transform = scale_dict_pnas,
+    batch_size=8192,
+    shuffle=True
+)                      
+
+time_max=4365  # last time step 
+#was previously calculated based on length of array in shuffle.nc divided by lat.size*lon.size
+#(8192 samples per time step) 
+
+train_X=np.nan*np.zeros((time_max,8192,64))
+train_Y=np.nan*np.zeros((time_max,8192,129))
+train_Y_emul=np.nan*np.zeros((time_max,8192,129))
+for i in tqdm(np.arange(train_X[:,1,1].size)):
+    
+    train_X[i],train_Y[i]=train_gen_IV[i]
+    train_Y_emul[i]=VAE_clim_clim_conv.predict(train_X[i])[:,0:129]
+    
+
+train_X_=np.reshape(train_X,(time_max*8192,64))
+train_Y_=train_gen_IV.output_transform.inverse_transform(np.reshape(train_Y,(time_max*8192,129)))
+train_Y_emul_=train_gen_IV.output_transform.inverse_transform(np.reshape(train_Y_emul,(time_max*8192,129)))
+
+train_mse=np.mean((train_gen_III.output_transform.transform(train_Y_emul_[:,0:65])-train_gen_III.output_transform.transform(train_Y_[:,0:65]))**2)
+print(train_mse)
+
+mean_bias_train=np.mean((train_gen_III.output_transform.transform(train_Y_emul_[:,0:65])-train_gen_III.output_transform.transform(train_Y_[:,0:65])))
+print(mean_bias_train)
+del train_X, train_Y, train_Y_emul
+
+## save MSEs and biases 
+Perf_array=ps.DataFrame({'test_mse':[mse],
+                            'test_bias':[mean_bias],
+                            'val_mse':[val_mse],
+                            'val_bias':[mean_bias_val],
+                            'train_mse':[train_mse],
+                            'train_bias':[mean_bias_train]})
+
+Perf_array.to_csv('perf_analy/VAE_clim_clim_conv_BN8_PNAS_scaling.csv')
